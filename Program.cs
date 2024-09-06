c2fa285f65bc61dfc627ccdf4484adde88420235
anisul-islam <anisul2010s@yahoo.co.uk>
2024-09-06 21:11:17 +0300

    Add Convert between Celsius and Fahrenheit.

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ Console.WriteLine($"Welcome to the unit converter App");
 Console.WriteLine($"\nAvailable Conversions:");
 
 // Kilometers to Miles
-Console.WriteLine($"Kilometers to Miles");
+Console.WriteLine($"1. Kilometers to Miles");
 Console.Write($"Enter the value you want to convert: ");
 inputValue = Console.ReadLine();
 value = Convert.ToDouble(inputValue);
@@ -13,12 +13,27 @@ convertedValue = value * 0.621371;
 Console.WriteLine($"{value} Kilometers = {convertedValue:F2} Miles");
 
 
-Console.WriteLine($"\n\nMiles to Kilometers");
+Console.WriteLine($"\n\n2. Miles to Kilometers");
 Console.Write($"Enter the value you want to convert: ");
 inputValue = Console.ReadLine();
 value = Convert.ToDouble(inputValue);
 convertedValue = value / 0.621371;
 Console.WriteLine($"{value} Miles = {convertedValue:F2} Kilometers");
+
+
+Console.WriteLine($"\n\n3. Celsius to Fahrenheit.");
+Console.Write($"Enter the value you want to convert: ");
+inputValue = Console.ReadLine();
+value = Convert.ToDouble(inputValue);
+convertedValue = (value * 1.8) + 32;
+Console.WriteLine($"{value} Celsius = {convertedValue:F2} Fahrenheit");
+
+Console.WriteLine($"\n\n4. Fahrenheit to Celsius");
+Console.Write($"Enter the value you want to convert: ");
+inputValue = Console.ReadLine();
+value = Convert.ToDouble(inputValue);
+convertedValue = (value - 32) / 1.8;
+Console.WriteLine($"{value} Fahrenheit = {convertedValue:F2} Celsius");
 
 
 
