27882cc013f8793b609cccbf2e585e4f74de7392
anisul-islam <anisul2010s@yahoo.co.uk>
2024-09-06 21:00:58 +0300

    Add Miles and Kilometers conversion

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string inputValue;
+double value, convertedValue;
+
+Console.WriteLine($"Welcome to the unit converter App");
+Console.WriteLine($"\nAvailable Conversions:");
+
+// Kilometers to Miles
+Console.WriteLine($"Kilometers to Miles");
+Console.Write($"Enter the value you want to convert: ");
+inputValue = Console.ReadLine();
+value = Convert.ToDouble(inputValue);
+convertedValue = value * 0.621371;
+Console.WriteLine($"{value} Kilometers = {convertedValue:F2} Miles");
+
+
+Console.WriteLine($"\n\nMiles to Kilometers");
+Console.Write($"Enter the value you want to convert: ");
+inputValue = Console.ReadLine();
+value = Convert.ToDouble(inputValue);
+convertedValue = value / 0.621371;
+Console.WriteLine($"{value} Miles = {convertedValue:F2} Kilometers");
+
+
+
+
