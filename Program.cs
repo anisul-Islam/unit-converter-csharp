037e365352515b45b947319005b9cfa38ecc1ba6
anisul-islam <anisul2010s@yahoo.co.uk>
2024-09-06 21:12:27 +0300

    Add Comments

--- Program.cs
+++ Program.cs
@@ -13,6 +13,7 @@ convertedValue = value * 0.621371;
 Console.WriteLine($"{value} Kilometers = {convertedValue:F2} Miles");
 
 
+// Miles to Kilometers
 Console.WriteLine($"\n\n2. Miles to Kilometers");
 Console.Write($"Enter the value you want to convert: ");
 inputValue = Console.ReadLine();
@@ -21,6 +22,7 @@ convertedValue = value / 0.621371;
 Console.WriteLine($"{value} Miles = {convertedValue:F2} Kilometers");
 
 
+//Celsius to Fahrenheit
 Console.WriteLine($"\n\n3. Celsius to Fahrenheit.");
 Console.Write($"Enter the value you want to convert: ");
 inputValue = Console.ReadLine();
@@ -28,6 +30,7 @@ value = Convert.ToDouble(inputValue);
 convertedValue = (value * 1.8) + 32;
 Console.WriteLine($"{value} Celsius = {convertedValue:F2} Fahrenheit");
 
+// Fahrenheit to Celsius
 Console.WriteLine($"\n\n4. Fahrenheit to Celsius");
 Console.Write($"Enter the value you want to convert: ");
 inputValue = Console.ReadLine();
