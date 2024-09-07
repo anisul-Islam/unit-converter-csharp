b18897a6ed757509dcb41100b345c14db40f98f5
anisul-islam <anisul2010s@yahoo.co.uk>
2024-09-07 23:17:22 +0300

    Add switch statement with multiple parameters

--- Program.cs
+++ Program.cs
@@ -15,49 +15,41 @@ Console.Write($"Enter the value you want to convert: ");
 inputValue = Console.ReadLine();
 value = Convert.ToDouble(inputValue);
 
-if (option == 1)
+switch ((option, value))
 {
-  // Kilometers to Miles
-  convertedValue = value * 0.621371;
-  Console.WriteLine($"{value} Kilometers = {convertedValue:F2} Miles");
-}
-else if (option == 2)
-{
-  // Miles to Kilometers
-  convertedValue = value / 0.621371;
-  Console.WriteLine($"{value} Miles = {convertedValue:F2} Kilometers");
-}
-else if (option == 3)
-{
-  //Celsius to Fahrenheit
-  convertedValue = (value * 1.8) + 32;
-  Console.WriteLine($"{value} Celsius = {convertedValue:F2} Fahrenheit");
-}
-else if (option == 4)
-{
-  // Fahrenheit to Celsius
-  convertedValue = (value - 32) / 1.8;
-  Console.WriteLine($"{value} Fahrenheit = {convertedValue:F2} Celsius");
-}
-else if (option == 5)
-{
-  // kilograms to pounds
-  convertedValue = value * 2.20462; // 1 kilogram = 2.20462 pounds
-  Console.WriteLine($"{value} kilograms = {convertedValue:F2} Pounds");
-}
-else if (option == 6)
-{
-  // Pounds to kilograms
-  convertedValue = value / 2.20462; // 1 Pound = 0.453592 kilograms
-  Console.WriteLine($"{value} Pounds = {convertedValue:F2} kilograms");
-}
-else
-{
-  Console.WriteLine($"Invalid Selection. Please run the program again.");
-}
+  case (1, var valueFromUser):
+    convertedValue = valueFromUser * 0.621371;
+    Console.WriteLine($"{valueFromUser} Kilometers = {convertedValue:F2} Miles");
+    break;
+  case (2, var valueFromUser):
+    // Miles to Kilometers
+    convertedValue = valueFromUser / 0.621371;
+    Console.WriteLine($"{valueFromUser} Miles = {convertedValue:F2} Kilometers");
+    break;
+  case (3, var valueFromUser):
+    //Celsius to Fahrenheit
+    convertedValue = (valueFromUser * 1.8) + 32;
+    Console.WriteLine($"{valueFromUser} Celsius = {convertedValue:F2} Fahrenheit");
+    break;
+  case (4, var valueFromUser):
+    // Fahrenheit to Celsius
+    convertedValue = (valueFromUser - 32) / 1.8;
+    Console.WriteLine($"{valueFromUser} Fahrenheit = {convertedValue:F2} Celsius");
+    break;
+  case (5, var valueFromUser):
+    // kilograms to pounds
+    convertedValue = valueFromUser * 2.20462; // 1 kilogram = 2.20462 pounds
+    Console.WriteLine($"{valueFromUser} kilograms = {convertedValue:F2} Pounds");
+    break;
+  case (6, var valueFromUser):
+    // Pounds to kilograms
+    convertedValue = valueFromUser / 2.20462; // 1 Pound = 0.453592 kilograms
+    Console.WriteLine($"{valueFromUser} Pounds = {convertedValue:F2} kilograms");
+    break;
+  default:
+    Console.WriteLine($"Invalid Selection. Please run the program again."); break;
 
-
-
+}
 
 
 
